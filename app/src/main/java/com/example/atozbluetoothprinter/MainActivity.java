9cb6d3c78ff3a90c77bb988be4ed5df8a0d3bb28
Muhammad Abdi Alidrus <muhammadabdi25@gmail.com>
2021-03-29 14:08:01 +0700

    create button that opens atozprinterlib activity

--- app/src/main/java/com/example/atozbluetoothprinter/MainActivity.java
+++ app/src/main/java/com/example/atozbluetoothprinter/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.atozbluetoothprinter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import com.example.atozbtprinterlib.AtozBtPrinter;
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MaterialButton btnOpen = findViewById(R.id.btn_open);
+        btnOpen.setOnClickListener(view -> AtozBtPrinter.initBluetoothPrinterManager(MainActivity.this)
+                .open());
     }
 }
